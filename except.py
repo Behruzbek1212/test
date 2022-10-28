1d21bd09d68dbc703721dcf68af83b5eab098345
Behruzbek1212 <begi8354@gmail.com>
2022-10-28 20:03:05 +0500

    exsept

---
+++ except.py
@@ -0,0 +1,11 @@
+try:
+    a = int(input("1-son: "))
+    b = int(input("2-son: "))
+    if a == 0:
+        print(a/b)
+        def xato(a):
+            return a
+    else:
+        print(b*a)
+except ZeroDivisionError:
+    print("Nol bo\'lish xatoligi")
